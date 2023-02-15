c6e5b8f0c2293658ffd1dca1dcded1cffc009636
Kabo-Wabo <supgev@gmail.com>
2023-02-15 13:50:13 +0000

    Работает БД

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ bot.use(stage.middleware())
 bot.start(ctx => {
     ctx.replyWithHTML('Приветсвую в <b>Moscowkranbot</b>. Великий Геворк работает над ним. Сейчас мы попробуем понять кто Вы')
 	let d = whoareyou(ctx.from.id);
-	if (d=='admin') {
+	if (d[0]=='admin') {
 		ctx.scene.enter('greeter')
-		ctx.replyWithHTML('Авторизация пройдена успешна. Вы в режиме <b>диспетчера</b>')
+		ctx.replyWithHTML('Авторизация пройдена успешна. Привет <b>'+d[1].name_telegram+'</b>! Вы в режиме <b>диспетчера</b>')
 	}
 		else {
 			ctx.reply('Вы не авторизованны')
@@ -26,14 +26,9 @@ bot.start(ctx => {
 
 
 
-
-
-
-
-
 bot.on('text', ctx => {
     try {
-	ctx.replyWithHTML(`Вы не авторизованы, обратитесь к администратору `)
+	ctx.replyWithHTML(`Вы не авторизованы, обратитесь к администратору @gevork_ch , или запустите бота заново командой /start`)
  	}
 	catch { console.err(error) }
 	})
--- controllers/check_id.js
+++ controllers/check_id.js
@@ -2,11 +2,18 @@ import {allfetch} from '../db.js'
 
 function whoareyou(x) {
 var user = "unknown"
+var params = ""
 
- if (x === 285512812 ) user = 'admin';
- if (x === 466427533 ) user = 'driver';
- if (x === 566427533 ) user = 'boss';
-return user;
+allfetch.forEach(function(entry) {
+
+ if (entry.id_telegram == x) {
+	if (entry.role == 1) user = 'admin'; params = entry ;
+	if (entry.role == 2) user = 'driver'; params = entry;
+	if (entry.role == 3) user = 'boss'; params = entry;
+ }
+
+});
+return [user,params];
 }
 
 export { whoareyou };
